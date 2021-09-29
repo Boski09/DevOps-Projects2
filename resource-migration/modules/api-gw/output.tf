7ceb1dc21de3978eb3156c720f9b131c3c58604a
sadeelanjum <sadeel094@gmail.com>
2021-09-29 12:24:56 +0500

    Clean up

--- resource-migration/modules/api-gw/output.tf
+++
@@ -1,6 +0,0 @@
-output "api_deployment_01" {
-    value = aws_api_gateway_deployment.api_deployment_01.invoke_url
-}
-output "api_deployment_02" {
-    value = aws_api_gateway_deployment.api_deployment_02.invoke_url
-}
